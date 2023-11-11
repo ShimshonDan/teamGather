8d60863d6c91caf3a3668221f89a57c81c168fba
Dima Eremeev <140871325+ShimshonDan@users.noreply.github.com>
2023-11-11 13:09:26 +0300

    Delete server directory

--- server/models/tokenModel.js
+++
@@ -1,8 +0,0 @@
-const {Schema, model} = require("moongoose")
-
-const TokenShema = Schema({
-   refreshToken:{type: String, required:true},
-   user:{type:Schema.Types.ObjectId, ref:"User"}
-});
-
-module.exports = model("User", TokenShema);
